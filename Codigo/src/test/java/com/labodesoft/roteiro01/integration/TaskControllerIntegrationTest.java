825c9c6b8736621146dfcfd7993b1d23725a757b
Gabriel Vitor <gabrielvitor0309@gmail.com>
2024-05-13 08:09:44 -0300

    bd

--- Codigo/src/test/java/com/labodesoft/roteiro01/integration/TaskControllerIntegrationTest.java
+++ Codigo/src/test/java/com/labodesoft/roteiro01/integration/TaskControllerIntegrationTest.java
@@ -11,7 +11,7 @@ import org.springframework.test.context.ActiveProfiles;
 import static io.restassured.RestAssured.get;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
-//
+
 
 @SpringBootTest(classes = {Roteiro01Application.class},  webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 @ActiveProfiles("test")
